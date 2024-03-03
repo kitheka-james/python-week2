c9e202ce6c4e9fcfb9141f568b39de274d977613
kitheka-james <kithekajames15@gmail.com>
2024-03-03 21:23:18 +0300

    first commit

---
+++ datatypes_assignment.py
@@ -0,0 +1,25 @@
+# creating an empty list
+my_list = []  
+
+#Append the following elements to my_list: 10, 20, 30, 40.
+my_list = [10,20,30,40]
+
+#Insert the value 15 at the second position in the list.
+my_list.insert(1,15)
+
+#Extend my_list with another list: [50, 60, 70].
+my_list.extend([50,60,70])
+
+#Remove the last element from my_list.
+my_list.pop()
+
+#Sort my_list in ascending order.
+my_list.sort()
+
+#Find and print the index of the value 30 in my_list.
+index_30 = my_list.index(30)
+
+print("The value of index_30 in my list is:", index_30)
+
+#Printing the entire list for reference
+print(my_list)
